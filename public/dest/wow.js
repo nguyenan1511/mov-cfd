a1a562f5a59532caa1d8afe98612916658e3e4eb
annguyen1511 <annguyen@wearetopgroup.com>
2024-04-03 13:39:51 +0700

    feat: fix wow offset

--- public/dest/wow.js
+++ public/dest/wow.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		var wow = new WOW({
 			boxClass: "wow", // animated element css class (default is wow)
 			animateClass: "animated", // animation css class (default is animated)
-			offset: 200, // distance to the element when triggering the animation (default is 0)
+			offset: 50, // distance to the element when triggering the animation (default is 0)
 			mobile: true, // trigger animations on mobile devices (default is true)
 			live: true,
 		});
