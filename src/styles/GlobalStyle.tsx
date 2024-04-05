49a022f31aefdb6fb2c3d3501b84c85bc3c479b5
annguyen1511 <annguyen@wearetopgroup.com>
2024-04-05 16:55:12 +0700

    feat: remove css reset

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import "antd/dist/reset.css";
+// import "antd/dist/reset.css";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@
 
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	corePlugins: {
+		preflight: false,
+	},
 	theme: {
 		fontFamily: {
 			gilroy: ["GilroyBold"],
